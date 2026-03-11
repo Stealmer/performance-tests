974e268128196eaf10ff6ce7d3197f0a9fab805a
Stealmer <stealmer.hd@gmail.com>
2026-03-11 19:50:30 +0500

    update

---
+++ clients/http/client.py
@@ -0,0 +1,30 @@
+from httpx import Client, URL, Response, QueryParams
+from typing import Any
+
+class HTTPClient:
+    """
+    Базовый HTTP API клиент, принимающий объект httpx.Client.
+
+    :param client: экземпляр httpx.Client для выполнения HTTP-запросов
+    """
+
+    def __init__(self, client: Client) -> None:
+        self.client = client
+
+    def get(self, url: URL | str, params: QueryParams | None = None) -> Response:
+        """
+        Выполняет GET-запрос.
+        :param url: URL-адрес эндпоинта.
+        :param params: GET-параметры запроса (например, ?key=value).
+        :return:  Объект Response с данными ответа.
+        """
+        return self.client.get(url, params=params)
+
+    def post(self, url: str, json: Any | None = None) -> Response:
+        """
+
+        :param url: URL-адрес эндпоинта.
+        :param json: Данные в формате json.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.post(url=url, json=json)
